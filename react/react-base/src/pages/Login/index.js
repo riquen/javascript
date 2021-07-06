46f9118c3bfed2ef2fb5bc634bd3194bd8a4dfd4
= <henri.arantes1@gmail.com>
2021-07-05 22:51:06 -0300

    Adicionando conteúdo

--- react/react-base/src/pages/Login/index.js
+++ react/react-base/src/pages/Login/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
 import { Container } from '../../styles/GlobalStyles';
 import { Title, Paragrafo } from './styled';
+import * as exampleActions from '../../store/modules/example/actions';
 
 export default function Login() {
+  const dispatch = useDispatch();
+
+  function handleCLick(e) {
+    e.preventDefault();
+
+    dispatch(exampleActions.clicaBotaoRequest());
+  }
+
   return (
     <Container>
       <Title>
@@ -11,7 +21,9 @@ export default function Login() {
         <small>Bem vindo</small>
       </Title>
       <Paragrafo>Lorem Ipsum</Paragrafo>
-      <button type="button">Enviar</button>
+      <button type="button" onClick={handleCLick}>
+        Enviar
+      </button>
     </Container>
   );
 }
